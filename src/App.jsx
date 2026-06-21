f57579e684d2e5536aedd7c529c8e69c8cbbc820
Olly <ollycodesdev@gmail.com>
2026-06-21 19:53:59 +0200

    refactor: how dummy data is assigned to character state

--- src/App.jsx
+++ src/App.jsx
@@ -2,70 +2,70 @@ import { useEffect, useState } from "react";
 
 function App() {
 
-  const [characters, setCharacters] = useState([]);
+  const [characters, setCharacters] = useState(
+    [
+      {
+        id: crypto.randomUUID(),
+        name: "Gon",
+        image: "/gon.webp"
+      },
+      {
+        id: crypto.randomUUID(),
+        name: "Goku",
+        image: "/gon.webp"
+      },
+      {
+        id: crypto.randomUUID(),
+        name: "Freeza",
+        image: "/gon.webp"
+      },
+      {
+        id: crypto.randomUUID(),
+        name: "Vegeta",
+        image: "/gon.webp"
+      },
+      {
+        id: crypto.randomUUID(),
+        name: "Gohan",
+        image: "/gon.webp"
+      },
+      {
+        id: crypto.randomUUID(),
+        name: "Krillin",
+        image: "/gon.webp"
+      },
+      {
+        id: crypto.randomUUID(),
+        name: "Tien",
+        image: "/gon.webp"
+      },
+      {
+        id: crypto.randomUUID(),
+        name: "Buu",
+        image: "/gon.webp"
+      },
+      {
+        id: crypto.randomUUID(),
+        name: "Nappa",
+        image: "/gon.webp"
+      },
+      {
+        id: crypto.randomUUID(),
+        name: "Raditz",
+        image: "/gon.webp"
+      },
+      {
+        id: crypto.randomUUID(),
+        name: "Android-18",
+        image: "/gon.webp"
+      },
+    ]
+  );
 
   const url = 'https://api.jikan.moe/v4/anime/11061/characters';
 
-  const dummyData = [
-    {
-      id: crypto.randomUUID(),
-      name: "Gon",
-      image: "/gon.webp"
-    },
-    {
-      id: crypto.randomUUID(),
-      name: "Goku",
-      image: "/gon.webp"
-    },
-    {
-      id: crypto.randomUUID(),
-      name: "Freeza",
-      image: "/gon.webp"
-    },
-    {
-      id: crypto.randomUUID(),
-      name: "Vegeta",
-      image: "/gon.webp"
-    },
-    {
-      id: crypto.randomUUID(),
-      name: "Gohan",
-      image: "/gon.webp"
-    },
-    {
-      id: crypto.randomUUID(),
-      name: "Krillin",
-      image: "/gon.webp"
-    },
-    {
-      id: crypto.randomUUID(),
-      name: "Tien",
-      image: "/gon.webp"
-    },
-    {
-      id: crypto.randomUUID(),
-      name: "Buu",
-      image: "/gon.webp"
-    },
-    {
-      id: crypto.randomUUID(),
-      name: "Nappa",
-      image: "/gon.webp"
-    },
-    {
-      id: crypto.randomUUID(),
-      name: "Raditz",
-      image: "/gon.webp"
-    },
-    {
-      id: crypto.randomUUID(),
-      name: "Android-18",
-      image: "/gon.webp"
-    },
-  ]
-
-  const shuffledChars = shuffle(dummyData);
-
+  const shuffledChars = shuffle(characters);
+  
   function shuffle(arr) {
     const chars = [...arr];
 
@@ -82,7 +82,7 @@ function App() {
       <h1>Hunter x Recall</h1>
       <ul>
         {shuffledChars.map((char) => {
-          return <li key={char.id}>
+          return <li onClick={() => handleClick(char.id)} key={char.id}>
             <img style={{ width: 100, height: 100 }} src={char.image} alt={char.name} />
             <p>Name: {char.name}</p>
           </li>
