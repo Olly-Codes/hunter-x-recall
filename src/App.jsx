da6d9b089bfb9be9453273c22fc9927eeb4ced46
Olly <ollycodesdev@gmail.com>
2026-06-21 21:07:32 +0200

    feat: add logic to handle tracking of character clicks

--- src/App.jsx
+++ src/App.jsx
@@ -2,60 +2,61 @@ import { useEffect, useState } from "react";
 
 function App() {
 
+  const [chosenCharacters, setChosenCharacters] = useState([]);
   const [characters, setCharacters] = useState(
     [
       {
-        id: crypto.randomUUID(),
+        id: 10,
         name: "Gon",
         image: "/gon.webp"
       },
       {
-        id: crypto.randomUUID(),
+        id: 20,
         name: "Goku",
         image: "/gon.webp"
       },
       {
-        id: crypto.randomUUID(),
+        id: 30,
         name: "Freeza",
         image: "/gon.webp"
       },
       {
-        id: crypto.randomUUID(),
+        id: 40,
         name: "Vegeta",
         image: "/gon.webp"
       },
       {
-        id: crypto.randomUUID(),
+        id: 50,
         name: "Gohan",
         image: "/gon.webp"
       },
       {
-        id: crypto.randomUUID(),
+        id: 89,
         name: "Krillin",
         image: "/gon.webp"
       },
       {
-        id: crypto.randomUUID(),
+        id: 90,
         name: "Tien",
         image: "/gon.webp"
       },
       {
-        id: crypto.randomUUID(),
+        id: 71,
         name: "Buu",
         image: "/gon.webp"
       },
       {
-        id: crypto.randomUUID(),
+        id: 87,
         name: "Nappa",
         image: "/gon.webp"
       },
       {
-        id: crypto.randomUUID(),
+        id: 101,
         name: "Raditz",
         image: "/gon.webp"
       },
       {
-        id: crypto.randomUUID(),
+        id: 3,
         name: "Android-18",
         image: "/gon.webp"
       },
@@ -64,24 +65,32 @@ function App() {
 
   const url = 'https://api.jikan.moe/v4/anime/11061/characters';
 
-  const shuffledChars = shuffle(characters);
-  
-  function shuffle(arr) {
-    const chars = [...arr];
+  function handleClick(id) {
+    if (chosenCharacters.includes(id)) {
+      alert("You already clicked on that character!");
+    } else {
+      setChosenCharacters((prevChars) => {
+        return [...prevChars, id ];
+      });
+      shuffle(characters);
+    }
+  }
 
-    for (let i = chars.length - 1; i > 0; i--) {
+  function shuffle(arr) {
+    let shuffledChars = [...arr];
+
+    for (let i = shuffledChars.length - 1; i > 0; i--) {
       const random = Math.floor(Math.random() * (i + 1));
 
-      [chars[i], chars[random]] = [chars[random], chars[i]];
+      [shuffledChars[i], shuffledChars[random]] = [shuffledChars[random], shuffledChars[i]];
     }
-
-    return chars;
+    setCharacters(shuffledChars);
   }
   return (
     <>
       <h1>Hunter x Recall</h1>
       <ul>
-        {shuffledChars.map((char) => {
+        {characters.map((char) => {
           return <li onClick={() => handleClick(char.id)} key={char.id}>
             <img style={{ width: 100, height: 100 }} src={char.image} alt={char.name} />
             <p>Name: {char.name}</p>
