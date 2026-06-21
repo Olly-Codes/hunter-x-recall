c00903078637bf5d2c300dcf43facb534368444a
Olly <ollycodesdev@gmail.com>
2026-06-21 18:54:43 +0200

    feat: add fetch data logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,27 @@
+import { useEffect, useState } from "react";
+
 function App() {
+
+  const [characters, setCharacters] = useState([]);
+
+  const url = 'https://api.jikan.moe/v4/anime/11061/characters';
+
+  useEffect(() => {
+    fetch(url)
+    .then((res) => res.json())
+    .then((result) => {
+      const top10 = result.data.slice(0, 10).map((char) => {
+        return {
+          id: char.character.mal_id,
+          name: char.character.name,
+          image: char.character.images.webp.image_url
+        }
+      });
+      
+      setCharacters(top10);
+    })
+    .catch((error) => console.error(error));
+  }, []);
 
   return (
     <>
